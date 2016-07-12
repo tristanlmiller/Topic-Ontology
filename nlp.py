8a84c8692610a1161ed6de2b2fc21b5fb01b5c05
Diya Das <diyadas@users.noreply.github.com>
2016-07-11 21:53:24 -0700

    first commit, set up to call parsing_xml, apply to text column of df only

---
+++ nlp.py
@@ -0,0 +1,15 @@
+"""
+Created on Mon Jul 11
+
+@author: Diya
+
+processing data in pandas data frame
+
+"""
+
+import parsing_xml
+import pandas
+
+def proc_text(df = parsing_xml.xml_to_df()):
+    df['process'] = df['text'].apply(lambda x: x[0:2])
+
