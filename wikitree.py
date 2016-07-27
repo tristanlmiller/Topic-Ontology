8488ab3fff49fe4bc271a2cbd67622dea16a0a8e
Diya Das <diyadas@users.noreply.github.com>
2016-07-26 19:59:58 -0700

    working tree document

--- wikitree.py
+++ wikitree.py
@@ -10,14 +10,16 @@ This script plots trees of our wikipedia data.
 
 from ete3 import Tree
 
-with open('/Users/diyadas/cdips/Topic-Ontology/SimpleWikiTree.txt','rb') as f:
-    treestr = f.read()    
+with open('/Users/diyadas/cdips/Topic-Ontology/SimpleWikiTree_u.txt','r') as f:
+    treestr = f.readlines()[0]    
 
-t = Tree( treestr )
+t = Tree( treestr.rstrip(),format=8)
 
 t.show()
 
 ## compare to
 
-t = Tree( "((a,b),c);" )
+t = Tree( '("[a,b]",c);' )
 t.show()
+
+# []
