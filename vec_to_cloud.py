2258823976050356165b625dc013c32755ac0334
Diya Das <diyadas@users.noreply.github.com>
2016-07-28 15:26:20 -0700

    generate wordclouds nb this maxes out the # of clouds for testing

--- vec_to_cloud.py
+++ vec_to_cloud.py
@@ -8,12 +8,17 @@ This script contains functions for generating word clouds.
 from wordcloud import WordCloud
 import numpy as np
 import pickle 
-import itertools
+import cluster_tree
+import matplotlib.pyplot as plt
 
 def vec2cloud(pkltf = 'Tfidf_Matrix.pkl',labpkl='Feature_List.pkl' ):
     word_array = vec2words(pkltf, labpkl)
-    
-    
+    word_array = word_array[0:20] #for testing
+    for i in range(len(word_array)):
+        wc = WordCloud(background_color="black")
+        wc = wc.generate(word_array[i])
+        wc.to_file("cloud"+str(i)+".png")
+
 
 def vec2words(pkltf,labpkl ):
     tf = pickle.load(open(pkltf,'rb')).toarray()
