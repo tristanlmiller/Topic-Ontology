c2d2afdaa1875c273ca4a0b43b90d459a84b34e7
Diya Das <diyadas@users.noreply.github.com>
2016-07-29 01:43:49 -0700

    add prefix so clouds from different trees aren't overwritten

--- vec_to_cloud.py
+++ vec_to_cloud.py
@@ -10,15 +10,16 @@ import numpy as np
 import pickle 
 import cluster_tree
 import matplotlib.pyplot as plt
+import os
 
-def vec2cloud(pkltf = 'Tfidf_Matrix.pkl',clabpkl = "c_labels.pkl", docpkl = "docs_in_cluster.pkl", wardtreepkl = "ward_tree.pkl", labpkl='features.pkl' ):
+def vec2cloud(pkltf = 'Tfidf_Matrix.pkl',clabpkl = "c_labels.pkl", docpkl = "docs_in_cluster.pkl", wardtreepkl = "ward_tree.pkl", labpkl='features.pkl',prefix='' ):
     word_array = vec2words(pkltf, labpkl, clabpkl, docpkl, wardtreepkl)
     word_array = word_array[0:20] #for testing
     for i in range(len(word_array)):
         wc = WordCloud(background_color='black')
         wc = wc.generate(word_array[i])
-        wc.to_file('cloud'+str(i)+'.png')
-        del wc #this was giving me issues earlier, so hopefully this fixes it
+        wc.to_file(prefix+'cloud'+str(i)+'.png')
+        del wc
 
 def vec2words(pkltf,labpkl,clabpkl, docpkl, wardtreepkl):
     tree= pickle.load(open(wardtreepkl,"rb"))
